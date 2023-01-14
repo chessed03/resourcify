746ee0f714142ec9fa52e5601b93c8c02535644e
chessed03 <chessed03@gmail.com>
2023-01-13 18:01:27 -0600

    new contact

--- app/Http/Livewire/ContactForm.php
+++ app/Http/Livewire/ContactForm.php
@@ -4,10 +4,10 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
-class ContacForm extends Component
+class ContactForm extends Component
 {
     public function render()
     {
-        return view('livewire.contac-form');
+        return view('livewire.contact-form.contact-form');
     }
 }
